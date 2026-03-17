8126663ab1016889aed81217714637511bdefa56
Olivia Nayak <olivia@Olivias-MacBook-Pro-3.local>
2026-03-17 18:02:02 -0500

    updated vite.config to contain outDir

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/p1"
+  base: "/p1",
+  build: {
+    outDir: "dist"
+  }
 })
