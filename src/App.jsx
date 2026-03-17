fb0fd2115aa45b9f83c8cb9e2839c46644299ce8
Olivia Nayak <olivia@Olivias-MacBook-Pro-3.local>
2026-03-17 18:15:08 -0500

    added an alert to the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import {Button} from 'react-bootstrap';
 
@@ -12,7 +9,7 @@ function App() {
         <h2>
           Welcome to Food Road Trip Generator!
         </h2>
-        <Button>
+        <Button onClick={() => alert("You have clicked the button! Stay tuned for updates!")}>
             Get Started Here!!!!
         </Button>
     </>
